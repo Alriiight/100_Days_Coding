5c1ab0378c611ec1020d7965b6e5732b500cbbdd
Alriiight <96583928+Alriiight@users.noreply.github.com>
2022-12-11 16:22:25 +0100

    f-string def and ex

--- notes.py
+++ notes.py
@@ -1,1 +1,11 @@
 # Math in python
+
+
+#f-string is an efficient way to convert data types within a single line of code in order to print to console. Use the following example for clarification:
+
+bmi = 27
+height = 1.80
+weight = 88
+is_overweight = True
+
+print(f"your height is {height} meters, your weight is {weight} kilograms. This means your BMI index is {bmi}. You are overweight is {is_overweight}.")
