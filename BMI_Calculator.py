b54e44729a11a385e2f7bb3b1a6fa991d99aa3c4
Alriiight <96583928+Alriiight@users.noreply.github.com>
2022-12-19 22:29:34 +0100

    Add comparison statements for BMI interpretation

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -35,3 +35,14 @@ print(bmi_as_int)
 # bmi = int(weight) / float(height) ** 2
 # bmi_as_int = int(bmi)
 # print(bmi_as_int)
+
+if bmi_as_int < 18.5:
+    print("You are underweight.")
+elif bmi_as_int < 25:
+    print("You have a normal weight.")
+elif bmi_as_int < 30:
+    print("You are overweight.")
+elif bmi_as_int < 35:
+    print("You are obese.")
+else: 
+    print("You are clinically obese.")
