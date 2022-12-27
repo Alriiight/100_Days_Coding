e47332ffa4344cd06116174a34a1990d03d811da
Alriiight <96583928+Alriiight@users.noreply.github.com>
2022-12-27 23:55:13 +0100

    Add comment with alternate method

--- Leap_Year.py
+++ Leap_Year.py
@@ -10,3 +10,16 @@ elif year % 100 != 0:
     print("Leap year.")
 elif year % 400 == 0:
     print("Leap year.")
+
+# Method checking for == 0.
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
