8feb5ef67eae610481c99e7d56f6e6b2b04ff076
Alriiight <96583928+Alriiight@users.noreply.github.com>
2022-12-28 00:14:34 +0100

    Delete previous changes

--- Rollercoaster_Tickets.py
+++ Rollercoaster_Tickets.py
@@ -3,11 +3,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-child_ticket = 5
-youth_ticket = 7
-adult_tickt = 12
-photo_cost = 5
-
 if height >= 120:
   print("You can ride the rollercoaster!")
   age = int(input("What is your age? "))
@@ -18,8 +13,6 @@ if height >= 120:
   else:
     print("Adult tickets are $12.")
 
-  input("Do you want a photo taken? Y or N. ")
-  if input == "Y":
-    print(f"Your total cost is {}")  
+  input("Do you want a photo taken? Y or N. ")  
 else:
   print("Sorry, you have to grow taller before you can ride.")
