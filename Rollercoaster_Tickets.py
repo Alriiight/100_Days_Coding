a57634f3e44ed2b6fadd67d36573e30e6cf13800
Alriiight <96583928+Alriiight@users.noreply.github.com>
2022-12-28 00:10:25 +0100

    Add pricing variables

--- Rollercoaster_Tickets.py
+++ Rollercoaster_Tickets.py
@@ -3,6 +3,11 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+child_ticket = 5
+youth_ticket = 7
+adult_tickt = 12
+photo_cost = 5
+
 if height >= 120:
   print("You can ride the rollercoaster!")
   age = int(input("What is your age? "))
@@ -12,5 +17,9 @@ if height >= 120:
     print("Youth tickets are $7.")
   else:
     print("Adult tickets are $12.")
+
+  input("Do you want a photo taken? Y or N. ")
+  if input == "Y":
+    print(f"Your total cost is {}")  
 else:
   print("Sorry, you have to grow taller before you can ride.")
