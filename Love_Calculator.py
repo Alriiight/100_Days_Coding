19f7c0782bb88f3ced0d3339369c97deef3a5303
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-01-05 18:21:04 +0100

    Count each letter and total for 'true"

--- Love_Calculator.py
+++ Love_Calculator.py
@@ -10,3 +10,15 @@ name2 = input("What is their name? \n")
 name1.lower()
 name2.lower()
 
+# Count for true
+num_t = name1.count("t") + name2.count("t")
+# print(num_t)
+num_r = name1.count("r") + name2.count("r")
+# print(num_r)
+num_u = name1.count("u") + name2.count("u")
+# print(num_u)
+num_e = name1.count("e") + name2.count("e")
+# print(num_e)
+
+num_true = num_t + num_r + num_u + num_e
+print(f"Your 'true' score is {num_true}")
