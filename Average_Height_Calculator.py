6348f9d49b12423266a52d32cd51b80236e39f8b
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-01-16 23:27:59 +0100

    Add comments explaining how len and sum work

--- Average_Height_Calculator.py
+++ Average_Height_Calculator.py
@@ -9,12 +9,12 @@ for n in range(0, len(student_heights)):
 
 total_height = 0
 for height in student_heights:
-     total_height += height
+     total_height += height # This is how sum() works.
 # print(total_height)
 
 total_students = 0
 for students in student_heights:
-    total_students += 1
+    total_students += 1 # This is how len() works.
 # print(total_students)
 
 average_height = total_height/total_students
