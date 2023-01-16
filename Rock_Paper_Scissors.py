36714fecee7d60ecb0bec79eca19e9be9189b6a6
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-01-16 17:41:37 +0100

    Change number data type and elif
    -To have more criteria in program logic, changed all numbers to int.
    -Before, numbers were converted to str because of original user input.
    -Player_choice is now converted to int instead
    -Add elif statement to detect when user inputs out of range number

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -29,31 +29,33 @@ scissors = '''
 import random
 
 # PLAYER CHOICE
-player_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if player_choice == "0":
+if player_choice == 0:
     print(rock)
-elif player_choice == "1":
+elif player_choice == 1:
     print(paper)
 else:
     print(scissors)
 
 # COMPUTER CHOICE
-computer_choice = str(random.randint(0, 2))
+computer_choice = random.randint(0, 2)
 print(computer_choice)
 
-if computer_choice == "0":
+if computer_choice == 0:
     print(rock)
-elif computer_choice == "1":
+elif computer_choice == 1:
     print(paper)
 else:
     print(scissors)
 
 # PROGRAM LOGIC
 
-if player_choice == "0" and computer_choice == "2" or player_choice == "1" and computer_choice == "0" or player_choice == "2" and computer_choice == "1":
+if player_choice == 0 and computer_choice == 2 or player_choice == 1 and computer_choice == 0 or player_choice == 2 and computer_choice == 1:
     print("You win!")
 elif player_choice == computer_choice:
     print("It's a draw")
+elif player_choice >= 3 or player_choice < 0:
+    print("You typed an invalid number, you lose")
 else:
     print("You lose")
