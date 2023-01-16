4cff5eb7e88cd439737ebe33caece50b414b1877
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-01-16 14:35:56 +0100

    Add location formula

--- Spot_Marker.py
+++ Spot_Marker.py
@@ -12,7 +12,7 @@ position = input("Where do you want to put the treasure? ")
 horizontal = int(position[0]) #2
 vertical = int(position[1]) #3
 
-
+map[vertical - 1][horizontal - 1] = "X"
 
 #Write your code above this row 👆
 
