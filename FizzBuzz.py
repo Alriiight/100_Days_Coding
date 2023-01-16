8be16c5a5647e281980753470446191f3217d790
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-01-17 00:23:28 +0100

    Add alternative method

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,6 +5,7 @@
 # When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
 #   And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
 
+# My original method
 for number in range(1, 101):
     if number % 3 == 0:
         if number % 5 == 0:
@@ -16,5 +17,16 @@ for number in range(1, 101):
             print("FizzBuzz")
         else:
             print("Buzz")
+    else:
+        print(number)
+
+# Alternative Method
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
     else:
         print(number)
