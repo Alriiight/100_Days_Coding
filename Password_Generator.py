66b26b61f59dbcd4b4db001636615f908c4ec897
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-01-17 18:06:15 +0100

    Create password list instead of string

--- Password_Generator.py
+++ Password_Generator.py
@@ -17,15 +17,28 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 # password = pass_let + pass_sym + pass_num
 # print(password)
 
-# Easy level using for loop
+# EASY LEVEL USING FOR LOOP
 
-password = ""
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(letters)
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
+# for char in range(1, nr_numbers + 1):
+#     password += random.choice(numbers)
+
+# print(password)
+
+# HARD LEVEL SOLUTION
+
+password_list = []
 
 for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
+    password_list += random.choice(letters)
 for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+    password_list += random.choice(symbols)
 for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+    password_list += random.choice(numbers)
 
-print(password)
+print(password_list)
