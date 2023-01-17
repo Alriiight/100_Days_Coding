0706b3a3b59cc6583cb2e57bcee925fec384d41a
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-01-17 18:20:13 +0100

    Shuffle password

--- Password_Generator.py
+++ Password_Generator.py
@@ -35,10 +35,11 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 password_list = []
 
 for char in range(1, nr_letters + 1):
-    password_list += random.choice(letters)
+    password_list.append(random.choice(letters))
 for char in range(1, nr_symbols + 1):
-    password_list += random.choice(symbols)
+    password_list.append(random.choice(symbols))
 for char in range(1, nr_numbers + 1):
-    password_list += random.choice(numbers)
+    password_list.append(random.choice(numbers))
 
+random.shuffle(password_list)
 print(password_list)
