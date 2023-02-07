25363820020eb1ef35d6be4428471e393774268c
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-02-07 12:54:58 +0100

    Remove letters list
    Program needs a word list instead of individual letters

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,10 @@
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 
 print("Welcome to Hangman!")
 print("Your goal is to guess the word before the character is hanged!")
 
 user_guess = input("Type a letter: ")
 print(user_guess)
+
