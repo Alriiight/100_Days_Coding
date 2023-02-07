938da5531921b994b5b339e4c8838278fd2ba33e
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-02-07 13:07:13 +0100

    Add random word variable
    - This variable is created to store the word selected for the game that the user must guess.
    - Word selection is done with random.choice module.

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,12 @@
 import random
 
-
+word_list = ["monkey", "ape", "car", "dog"]
 
 print("Welcome to Hangman!")
 print("Your goal is to guess the word before the character is hanged!")
+
+ran_word = random.choice(word_list)
+print(ran_word)
 
 user_guess = input("Type a letter: ")
 print(user_guess)
