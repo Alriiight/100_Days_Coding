7aa17011bda76d1cc9b26d0f0628c248780785e6
Alriiight <96583928+Alriiight@users.noreply.github.com>
2023-02-16 17:17:42 +0100

    Reorganize code
    Moved variables together for better future readability.

--- Hangman.py
+++ Hangman.py
@@ -2,16 +2,13 @@
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-
-
 chosen_word = random.choice(word_list) # This variable stores the random word selected from the list above by the program. 
+word_length = len(chosen_word) # Since I was using the len() multiple times, I created this variable for better readability and less typing.0
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = [] # I create this empty list in order to store the "spaces" for each letter and its position. It is used to show the user the empty spaces as underscores intead of the actual letters.
-
-word_length = len(chosen_word) # Since I was using the len() multiple times, I created this variable for better readability and less typing.0
 
 for _ in range(word_length): 
   display += "_"
